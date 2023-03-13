e62c86db8acd3bd1680cfe81fd0b351d37ef4e54
Annd <ssandreia30@gmail.com>
2023-03-13 17:15:44 -0300

    Add script.js file and declaration of variables

---
+++ script.js
@@ -0,0 +1,10 @@
+//variables declaration
+
+const btnConvert = document.getElementById("btnConvert");
+const btnClean = document.getElementById("btnClean");
+
+let celsius = document.querySelector(".celsius-value");
+let fahrenheit = document.querySelector(".fahrenheit-value");
+let kelvin = document.querySelector(".kelvin-value");
+
+
