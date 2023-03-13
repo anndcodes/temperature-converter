2c7d2d90fed10d81244875450081dbf54b8d0762
Annd <ssandreia30@gmail.com>
2023-03-13 17:58:26 -0300

    Add convert button function for convertion

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//variables declaration
+////////// variables declaration
 
 const btnConvert = document.getElementById("btnConvert");
 const btnClean = document.getElementById("btnClean");
@@ -8,3 +8,31 @@ let fahrenheit = document.querySelector(".fahrenheit-value");
 let kelvin = document.querySelector(".kelvin-value");
 
 
+///////// btnConvert function
+
+btnConvert.addEventListener('click', () => {
+  if(celsius.value) {
+    let fahrenheitValue = (celsius.value * 9/5) + 32;
+    fahrenheit.value = fahrenheit.textContent = fahrenheitValue.toFixed(1) + " °F";
+    /////
+    let kelvinValue = (celsius.value * 1) + 273.15;
+    kelvin.value = kelvin.textContent = kelvinValue.toFixed(1) + " K";
+
+  } else if(fahrenheit.value) {
+    let celsiusValue = (fahrenheit.value - 32) * 5/9;
+    celsius.value = celsius.textContent = celsiusValue.toFixed(1) + " °C";
+    /////
+    let kelvinValue = (fahrenheit.value - 32) * 5/9 + 273.15;
+    kelvin.value = kelvin.textContent = kelvinValue.toFixed(1) + " K";
+
+  } else if(kelvin.value) {
+    let celsiusValue = kelvin.value - 273.15;
+    celsius.value = celsius.textContent = celsiusValue.toFixed(1) + " °C";
+    /////
+    let fahrenheitValue = (kelvin.value - 273.15) * 9/5 + 32;
+    fahrenheit.value = fahrenheit.textContent = fahrenheitValue.toFixed(1) + " °F"
+
+  } else {
+    
+  }
+}) 
